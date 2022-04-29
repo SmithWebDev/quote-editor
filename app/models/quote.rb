047364fda745fb35be1b8887d65943bec7512332
SmithWebDev <smithwebdd@gmail.com>
2022-04-29 03:00:53 -0400

    added broadcast action helper and broadcast transaction helper

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,8 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+  after_create_commit lambda {
+                        broadcast_prepend_to 'quotes', partial: 'quotes/quote', locals: { quotes: self },
+                                                       target: 'quotes'
+                      }
 end
