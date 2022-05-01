0416a286781b2ab2e2716ec81150b2aedad69172
SmithWebDev <smithwebdd@gmail.com>
2022-05-01 11:37:57 -0400

    added nav styling, refactored company/user info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :devise_controller?
-  before_action -> { sleep 1.5 }
+
+  private
+
+  def current_company
+    @current_company ||= current_user.company if user_signed_in?
+  end
+  helper_method :current_company
 end
