5f8f1a1787623bf16f15cfd53f8667076b521c42
SmithWebDev <smithwebdd@gmail.com>
2022-05-02 01:11:32 -0400

    Disabled turbo to build flash message component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 import "./controllers"
+// Disabled Turbo to begin working on standard flash messages
+Turbo.session.drive = false
