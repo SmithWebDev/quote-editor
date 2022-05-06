abd67910bb0d0e79e9e264f475304f7591acbee9
SmithWebDev <smithwebdd@gmail.com>
2022-05-06 14:11:26 -0400

    updated quote model

--- app/models/line_item_date.rb
+++ app/models/line_item_date.rb
@@ -1,3 +1,9 @@
 class LineItemDate < ApplicationRecord
   belongs_to :quote
+
+  validates :date,
+            presence: true,
+            uniqueness: { scope: :quote_id }
+
+  scope :ordered, -> { order(date: :asc) }
 end
