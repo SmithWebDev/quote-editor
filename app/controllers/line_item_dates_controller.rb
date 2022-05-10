9867626a8a9f91fce488c5231d44f48ca22b4dd0
SmithWebDev <smithwebdd@gmail.com>
2022-05-10 15:33:11 -0400

    updated quotes show page with link to LineItemDate turbo_frame, added create turbo_stream for line_item_dates, updated format in line_item_dates controller

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -13,7 +13,8 @@ class LineItemDatesController < ApplicationController
 
     respond_to do |format|
       if @line_item_date.save
-        format.html { redirect_to quote_path(@quote), notice: 'Line item date was successfully created.' }
+        format.html { redirect_to quote_path(@quote), notice: 'Date was successfully created.' }
+        format.turbo_stream { flash.now[:notice] = 'Date was successfully created.' }
       else
         render :new, status: :unprocessable_entity
       end
@@ -22,7 +23,10 @@ class LineItemDatesController < ApplicationController
 
   def update
     if @line_item_date.update(line_item_date_params)
-      redirect_to quote_path(@quote), notice: 'Date was successfully updated.'
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: 'Date was successfully updated.' }
+        format.turbo_stream { flash.now[:notice] = 'Date was successfully updated.' }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -34,6 +38,7 @@ class LineItemDatesController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to quote_path(@quote), notice: 'Date was successfully destroyed.' }
+      format.turbo_stream { flash.now[:notice] = 'Date was successfully destroyed.' }
     end
   end
 
