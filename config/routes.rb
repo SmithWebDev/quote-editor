b967b8d5a4eaff55f0f00f803d4e6035fa057ff2
SmithWebDev <smithwebdd@gmail.com>
2022-05-11 19:52:47 -0400

    created crud setup for line item, added line item dates wrapper for line items

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'line_item/new'
-  get 'line_item/create'
   resources :line_item_dates
-  root to: 'pages#home'
+  root to: "pages#home"
   devise_for :users
   resources :quotes do
     resources :line_item_dates, except: %i[index show] do
       resources :line_items, except: %i[index show]
     end
   end
-  get 'quotes/index'
+  get "quotes/index"
 end
