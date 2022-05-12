45e64f995c88872705e9719a9a742598bf179dc4
SmithWebDev <smithwebdd@gmail.com>
2022-05-11 20:06:35 -0400

    corrected line items controller naming, updated form attribute

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,7 @@
-class LineItemController < ApplicationController
+class LineItemsController < ApplicationController
+  before_action :set_quote
+  before_action :set_line_item_date
+
   def new
     @line_item = @line_item_date.line_items.build
   end
@@ -16,14 +19,14 @@ class LineItemController < ApplicationController
   private
 
   def line_item_params
-  params.require(:line_item).permit(:name, :description, :quantity, :unit_price)
+    params.require(:line_item).permit(:name, :description, :quantity, :unit_price)
   end
 
   def set_quote
-  @quote = current_company.quotes.find(params[:quote_id])
+    @quote = current_company.quotes.find(params[:quote_id])
   end
 
   def set_line_item_date
-  @line_item_date = @quote.line_item_dates.find(params[:line_item_date_id])
+    @line_item_date = @quote.line_item_dates.find(params[:line_item_date_id])
   end
 end
