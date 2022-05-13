3bad01c21664a9b1ea03eab75110bea2969fa134
SmithWebDev <smithwebdd@gmail.com>
2022-05-13 00:14:26 -0400

    added quote total partial and styling

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -18,6 +18,7 @@
 #
 class Quote < ApplicationRecord
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
   belongs_to :company
   validates :name, presence: true
 
@@ -33,4 +34,8 @@ class Quote < ApplicationRecord
 
   # previous 3 calls are refactored to just a single line
   broadcasts_to ->(quote) { [quote.company, 'quotes'] }, inserts_by: :prepend
+
+  def total_price
+  line_items.sum(&:total_price)
+  end
 end
